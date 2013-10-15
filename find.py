656ea11d72a7aa0c2f3a60970d6690482637c2b8
Matt Taylor <taylor@kensho.com>
2013-10-15 13:34:24 -0400

    Add positional arg for regex

--- find.py
+++ find.py
@@ -5,7 +5,8 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='src_finder')
-    parser.add_argument('-p', '--pattern',   help='file pattern to match',             default='*')
+    parser.add_argument("regex_positional",  help='egrep regex (overrides -r)',        default='',                      nargs='?')
+    parser.add_argument('-p', '--pattern',   help='filename pattern to match',         default='*')
     parser.add_argument('-r', '--regex',     help='egrep regex for in files')
     parser.add_argument('-x', '--exclude',   help='egrep exclude regex',               default='\.git|site-pack|\.pyc')
     parser.add_argument('-d', '--dir',       help='directory to find in',              default=os.getcwd())
@@ -13,12 +14,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if not args.regex and args.pattern=='*':
+    if not args.regex_positional and not args.regex and args.pattern=='*':
         parser.error("Must supply a regex or pattern")
 
+    regex = args.regex_positional if args.regex_positional else args.regex
+    print "positional: %s, option: %s --> %s" % (args.regex_positional, args.regex, regex)
     cmd = '/usr/bin/find %s -type %s -iname "%s" | /usr/bin/egrep -v "%s"' % (args.dir, args.type, args.pattern, args.exclude)
-    if args.regex:
-        cmd += ' | /usr/bin/xargs /usr/bin/egrep "%s"' % (args.regex)
+    if regex:
+        cmd += ' | /usr/bin/xargs /usr/bin/egrep "%s"' % (regex)
 
     print "cmd: %s" % (cmd)
-    os.system(cmd)
+    #os.system(cmd)
